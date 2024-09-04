c6360cd87f4579c122c922307d8f0ccb16040f17
Juan Villafuerte Mollinedo <villafuerte.j@outlook.com>
2024-09-03 22:42:40 -0400

    Integración con postgresql, creacion de las entidades producto y Compra

--- src/main/java/com/villa/java_market/controller/HelloController.java
+++ src/main/java/com/villa/java_market/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.villa.java_market;
+package com.villa.java_market.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
